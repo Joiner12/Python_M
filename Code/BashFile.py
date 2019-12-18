7301113ca836e425cc158fac8f2dc90a71dbb191
Risky Junior <risky@qq.com>
2019-12-18 16:18:50 +0800

    bash 指令执行

---
+++ Code/BashFile.py
@@ -0,0 +1,27 @@
+#-*- coding:utf-8 -*-
+
+import os
+import shutil
+
+
+def BashFile():
+    tar_path = r"C:\Users\Whtest\Desktop\Draft"
+    # listdir function
+    detail = os.listdir(tar_path)
+    if len(detail) > 0:
+        for i in detail:
+            pass
+            # print(i)
+    # walk function
+    detail_1 = os.walk(tar_path)
+    for (path,dirnames,filenames) in detail_1:
+        for i in dirnames:
+            print(i)
+        for j in filenames:
+            print(j)
+
+
+if __name__ == "__main__":
+    print("bash file handle")
+    BashFile()
+    
